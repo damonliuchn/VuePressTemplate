b972f0e6f816203de6f6f1db0ed4a06d39a73786
Mason Liu <MasonLiuChn@gmail.com>
2020-08-13 14:28:07 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,36 @@
+const fs = require('fs')
+const path = require('path')
+
+function getChildren(path) {
+  const root = []
+  readDirSync(path,root)
+  return root
+}
+
+function readDirSync(path,root){
+  var pa = fs.readdirSync(path);
+  pa.forEach(function(ele,index){
+    var info = fs.statSync(path+"/"+ele)
+    if(info.isDirectory()) {
+      readDirSync(path+ele,root)
+    } else {
+      if (checkFileType(ele)) {
+        root.push(prefixPath(path,ele))
+      }
+    }
+  })
+}
+
+function checkFileType(path) {
+  return path.includes(".md")
+}
+
+function prefixPath(basePath,dirPath) {
+  let index = basePath.indexOf("/")
+  basePath = basePath.slice(index,path.length)
+  return path.join(basePath,dirPath).replace(/\\/g,"/")
+}
+
 module.exports = {
     configureWebpack: {
         resolve: {
